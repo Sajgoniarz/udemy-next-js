8ac8f8c734d3c68d29248d5c6236ea82d4d7d2ed
Marek Naharnowicz <Sajgoniarz1@gmail.com>
2024-04-11 17:18:42 +0200

    Display snippets from db at Home Page

--- snippets/src/app/page.tsx
+++ snippets/src/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import {db} from "@/db/client";
 
-export default function Home() {
-  return (
-    <main>
-        Home Page
-    </main>
-  );
+export default async function Home() {
+
+    const snippets = await db.snippet.findMany();
+
+    const renderedSnippets = snippets.map(s => (
+        <div key={s.id}>{s.title}</div>
+    ));
+
+    return (
+        <main>
+            Home Page
+            {renderedSnippets}
+        </main>
+    );
 }
