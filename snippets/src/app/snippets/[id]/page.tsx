e453e34b9113c613c84897d575c5e93c77c87159
Marek Naharnowicz <Sajgoniarz1@gmail.com>
2024-04-11 18:25:24 +0200

    Add displaying snippet

---
+++ snippets/src/app/snippets/[id]/page.tsx
@@ -0,0 +1,21 @@
+import dbClient from '@/db/client';
+import {notFound} from "next/navigation";
+
+interface SnippetEditProps {
+    params: {
+        id: string,
+    }
+}
+export default async function SnippetEditPage(props: SnippetEditProps){
+    const snippet = await dbClient.snippet.findFirst({
+        where: {
+            id: parseInt(props.params.id)
+        }
+    });
+
+    if(!snippet){
+        return notFound();
+    }
+
+    return <div>Edit snippet: {snippet.title}</div>
+}
