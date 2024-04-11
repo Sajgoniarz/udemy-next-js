d4c7ef3b26fb6ca85d15e69e1c14e31da187f915
Marek Naharnowicz <Sajgoniarz1@gmail.com>
2024-04-11 18:57:10 +0200

    Style Home Page and add links to snippets

--- snippets/src/app/page.tsx
+++ snippets/src/app/page.tsx
@@ -1,18 +1,29 @@
 import Image from "next/image";
 import dbClient from "@/db/client";
+import Link from "next/link";
 
 export default async function Home() {
 
     const snippets = await dbClient.snippet.findMany();
 
     const renderedSnippets = snippets.map(s => (
-        <div key={s.id}>{s.title}</div>
+        <Link key={s.id}
+            className="flex justify-between items-center p-2 border rounded"
+            href={`/snippets/${s.id}`}
+        >
+            {s.title}
+        </Link>
     ));
 
     return (
         <main>
-            Home Page
-            {renderedSnippets}
+            <div className="flex m-2 justify-between items-center">
+                <h1 className="text-xl font-bold">Snippets</h1>
+                <Link className="border rounded p-2" href="snippets/new">New</Link>
+            </div>
+            <div className="flex gap-2 flex-col">
+                {renderedSnippets}
+            </div>
         </main>
     );
 }
