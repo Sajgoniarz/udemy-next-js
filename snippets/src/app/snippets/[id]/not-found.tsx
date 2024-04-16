508a326cd56dba2a2aa60253cb563c1d36f4dee5
Marek Naharnowicz <Sajgoniarz1@gmail.com>
2024-04-16 11:31:00 +0200

    Lint fix

--- snippets/src/app/snippets/[id]/not-found.tsx
+++ snippets/src/app/snippets/[id]/not-found.tsx
@@ -1,7 +1,7 @@
 export default function SnippetNotFoundPage() {
     return (
         <div className="text-xl bold">
-            <h1>Sorry, but we couldn't find your snippet.</h1>
+            <h1>Sorry, but we couldn&apos;t find your snippet.</h1>
         </div>
     )
 }
