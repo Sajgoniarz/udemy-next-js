d0e2e9ddd0615b0ec7cef78e550ace32e0dc746f
Marek Naharnowicz <Sajgoniarz1@gmail.com>
2024-04-27 14:11:50 +0200

    Add path helper

---
+++ discuss/src/path.ts
@@ -0,0 +1,8 @@
+﻿const paths = {
+    home: () => "/",           
+    topicShow: (slug: string) => `/topics/${slug}`,
+    postShow:(topicSlug: string, postId: string) => `/topics/${topicSlug}/posts/${postId}`,
+    postCreate:(topicSlug: string) => `/topics/${topicSlug}/posts/new`,
+}
+
+export default paths;
