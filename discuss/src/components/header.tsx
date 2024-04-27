020206f11c4df58e6cd7f43d0274405f59881074
Marek Naharnowicz <Sajgoniarz1@gmail.com>
2024-04-27 18:27:35 +0200

    Add Header component

---
+++ discuss/src/components/header.tsx
@@ -0,0 +1,27 @@
+﻿import Link from "next/link";
+import {
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    Input,
+} from "@nextui-org/react";
+import UserProfile from "@/components/userProfile";
+
+export default async function Header() {
+    return (
+        <Navbar className="shadow mb-6">
+            <NavbarBrand>
+                <Link href="/" className="font-bold">Discuss</Link>
+            </NavbarBrand>
+            <NavbarContent justify="center">
+                <NavbarItem>
+                    <Input/>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarContent justify="end">
+                <UserProfile/>
+            </NavbarContent>
+        </Navbar>
+    );
+}
---
+++ discuss/src/components/userProfile.tsx
@@ -0,0 +1,33 @@
+﻿import {auth} from "@/auth";
+import {Avatar, Button, NavbarItem, Popover, PopoverContent, PopoverTrigger} from "@nextui-org/react";
+import * as actions from "@/actions";
+
+export default async function UserProfile() {
+
+    const session = await auth();
+
+    const authenticatedUser: React.ReactNode =
+        <Popover placement="bottom">
+            <PopoverTrigger>
+                <Avatar src={session?.user?.image || ''}/>
+            </PopoverTrigger>
+            <PopoverContent>
+                <form action={actions.signOut} className="p-4">
+                    <Button type="submit">Sign Out</Button>
+                </form>
+            </PopoverContent>
+        </Popover>
+
+    const anonymousUser: React.ReactNode =
+        <form action={actions.signIn}>
+            <NavbarItem>
+                <Button type="submit" color="secondary" variant="bordered">Sign in</Button>
+            </NavbarItem>
+        </form>;
+
+    return (
+        session?.user
+            ? authenticatedUser
+            : anonymousUser
+    );
+}
