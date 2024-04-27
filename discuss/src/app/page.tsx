7061614e47dfc00106a5350aa88bd81f3fb4f7c6
Marek Naharnowicz <Sajgoniarz1@gmail.com>
2024-04-27 18:48:27 +0200

    Clean up Home component

--- discuss/src/app/page.tsx
+++ discuss/src/app/page.tsx
@@ -1,27 +1,3 @@
-import {Button} from "@nextui-org/react";
-import * as actions from "@/actions";
-import {auth} from "@/auth"
-import Profile from "@/components/profile";
-
-export default async function Home() {
-    const session = await auth();
-
-    return (
-        <>
-            <form action={actions.signIn}>
-                <Button type="submit">Sign in</Button>
-            </form>
-            <form action={actions.signOut}>
-                <Button type="submit">Sign out</Button>
-            </form>
-            <div>
-                {session?.user
-                    ? JSON.stringify(session.user)
-                    : "signed off"
-                }
-            </div>
-
-            <Profile />
-        </>
-    );
+export default function Home() {
+    
 }
