02c89adbaf595ea9efdc99bcbb4e4becac882447
Marek Naharnowicz <Sajgoniarz1@gmail.com>
2024-05-04 13:33:28 +0200

    Paste "boring" components

--- discuss/src/app/topics/[slug]/posts/[id]/page.tsx
+++ discuss/src/app/topics/[slug]/posts/[id]/page.tsx
@@ -1,7 +1,27 @@
-﻿const PostShowPage = () => {
-    return (
-        <div>Show post</div>
-    )
-};
+import Link from "next/link";
+import PostShow from "@/components/posts/post-show";
+import CommentList from "@/components/comments/comment-list";
+import CommentCreateForm from "@/components/comments/comment-create-form";
+import paths from "@/path";
 
-export default PostShowPage;
+interface PostShowPageProps {
+  params: {
+    slug: string;
+    postId: string;
+  };
+}
+
+export default async function PostShowPage({ params }: PostShowPageProps) {
+  const { slug, postId } = params;
+
+  return (
+    <div className="space-y-3">
+      <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
+        {"< "}Back to {slug}
+      </Link>
+      {/* <PostShow /> */}
+      {/* <CommentCreateForm postId={postId} startOpen /> */}
+      {/* <CommentList comments={comments} /> */}
+    </div>
+  );
+}
