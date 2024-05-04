0561c2ca9e6d063ff7dcebaa43c20157713fe6b0
Marek Naharnowicz <Sajgoniarz1@gmail.com>
2024-05-04 16:34:23 +0200

    Finish Post Page

--- discuss/src/components/posts/post-show.tsx
+++ discuss/src/components/posts/post-show.tsx
@@ -1,10 +1,22 @@
-interface PostShowProps {}
+import NotFound from "next/dist/client/components/not-found-error";
 
-export default function PostShow({}: PostShowProps) {
-  return (
-    <div className="m-4">
-      <h1 className="text-2xl font-bold my-2">{post.title}</h1>
-      <p className="p-4 border rounded">{post.content}</p>
-    </div>
-  );
+interface PostShowProps {
+    id: string,
+}
+
+export default async function PostShow({id}: PostShowProps) {
+    const post = await dbClient.post.findFirst({
+        where: {id}
+    });
+    
+    if(!post){
+        NotFound();
+    }
+    
+    return (
+        <div className="m-4">
+            <h1 className="text-2xl font-bold my-2">{post.title}</h1>
+            <p className="p-4 border rounded">{post.content}</p>
+        </div>
+    );
 }
